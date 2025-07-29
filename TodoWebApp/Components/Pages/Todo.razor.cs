56e248da85c9addb5582cbc1a934fd2938b768ef
EIB-Sahil <Sahil.Dhull@acertusdelivers.com>
2025-07-29 16:01:06 +0530

    Add project files.

---
+++ TodoWebApp/Components/Pages/Todo.razor.cs
@@ -0,0 +1,22 @@
+﻿using TodoWebApp.Models;
+using System.Collections.Generic;
+//using Microsoft.AspNetCore.Components.QuickGrid
+
+namespace TodoWebApp.Components.Pages
+{
+    public partial class Todo
+    {
+        private List<TodoModel> todos = [];
+        private string newTodoItem = "";
+        //private PaginationState pagination = new() { ItemsPerPage = 10 };
+
+        private void AddTodoItem()
+        {
+            if (!string.IsNullOrWhiteSpace(newTodoItem))
+            {
+                todos.Add(new TodoModel { Title = newTodoItem });
+                newTodoItem = string.Empty;
+            }
+        }
+    }
+}
---
+++ TodoWebApp/Models/TodoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoWebApp.Models
+{
+    public class TodoModel
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public bool IsDone { get; set; }
+    }
+}
